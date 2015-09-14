7dff62b3f1b7656e23c8e71ce5bb39bb0fe4ed81
jcadam <james@jamesadam.me>
2015-09-13 20:49:19 -0400

    added notifications service to build script

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -22,17 +22,21 @@ mkdir $BUILD_DIR
 mkdir ${BUILD_DIR}/users
 mkdir ${BUILD_DIR}/scripts
 mkdir ${BUILD_DIR}/wikis
+mkdir ${BUILD_DIR}/notifications
 mkdir ${BUILD_DIR}/frontend
 mkdir ${BUILD_DIR}/frontend/web_app
 # Build stuff
 go build -v -o ${BUILD_DIR}/users/users ../users
 go build -v -o ${BUILD_DIR}/wikis/wikis ../wikis 
+go build -v -o ${BUILD_DIR}/notifications/notifications ../notifications
 go build -v -o ${BUILD_DIR}/frontend/frontend ../frontend 
 # Copy some supporting files
 cp ../users/config.ini.example ${BUILD_DIR}/users/config.ini.example
 cp ../wikis/config.ini.example ${BUILD_DIR}/wikis/config.ini.example
+cp ../notifications/config.ini.example ${BUILD_DIR}/notifications/config.ini.example
 cp ../frontend/config.ini.example ${BUILD_DIR}/frontend/config.ini.example
 cp -R ../frontend/plugins ${BUILD_DIR}/frontend/plugins
+cp -R ../notifications/templates ${BUILD_DIR}/notifications/templates
 # "Compile" the webapp, then copy it to the build dir
 r.js -o ../frontend/web_app/app/scripts/app.build.js
 cp -R ../frontend/web_app/wikifeat-build ${BUILD_DIR}/frontend/web_app/app
